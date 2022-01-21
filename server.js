f14f19a44e8c4a340838625642fe6f6c7ce39bc1
sirben181 <bensonkamauben@gmail.com>
2022-01-21 17:34:57 +0300

    editted

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const dotenv=require('dotenv')
 const cors =require('cors')
 const connectDB=require('./config/db')
 
-const app=express()
+
 dotenv.config({path:'./config/config.env'})
-const corsOptions = {
-    origin: 'http://localhost:3000/',
-    optionsSuccessStatus: 200}
+
 connectDB();
+
+const app=express()
 const PORT=process.env.PORT || 5000
 
 app.use(cors())
  
 
 app.use('/',require('./routes/index'))
-app.use('/posts',cors(corsOptions),require('./routes/index'))
+app.use('/posts',require('./routes/index'))
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
