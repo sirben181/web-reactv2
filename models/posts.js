e68a2e053fe91e868a7f71c2e82e4177a03f57ff
sirben181 <bensonkamauben@gmail.com>
2022-02-10 22:42:13 +0300

    form completed

--- models/posts.js
+++ models/posts.js
@@ -2,11 +2,12 @@ const mongoose=require('mongoose')
 
 const postSchema=  new mongoose.Schema({
     title:{type:String, required:true},
-    createdAt:{type: Date, 
-        default: (new Date().toLocaleDateString())},
+    // createdAt:{type: Date, 
+    //     default: (new Date().toLocaleDateString())},
     author:{type: String, required:true},
     description:{type: String, require:true},
-    body:{type: String, required:true}
+    markdown:{type: String, required:true},
+    timestamps:{type: Date, required:true}
     
 
 }) 
--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(express.urlencoded({
-    extended: false
-}));
+    extended: false}));
 //
 app.get('/',(req,res,next)=>{
     res.render('home')
